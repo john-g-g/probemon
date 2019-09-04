b322222e20e9bdddd598120145217f109a8cc4eb
John Gerlock <john.gerlock@gmail.com>
2019-09-04 11:32:09 -0400

    Initial commit

--- probemon.py
+++ probemon.py
@@ -1,96 +1,121 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import time
 import datetime
 import argparse
 import netaddr
 import sys
+import time
+import os
 import logging
+import random
 from scapy.all import *
-from pprint import pprint
-from logging.handlers import RotatingFileHandler
+from functools import partial
 
+import signal
+from multiprocessing import Process
 
-NAME = 'probemon'
+logging.basicConfig(format='%(asctime)s\t%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
-DEBUG = False
+def unused_channel(channels):
+    channel = random.randrange(1,12)
+    while channel in channels:
+        channel = random.randrange(1,12)
+    return channel
 
-def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
-	def packet_callback(packet):
-		
-		if not packet.haslayer(Dot11):
-			return
+def channel_hopper(interfaces, interval):
+    while True:
+        try:
+            channels = []
+            for interface in interfaces:
+                channel = unused_channel(channels)
+                channels.append(channel)
+                logger.info('hopping to channel %s on %s',channel, interface)
+                os.system(f'iwconfig {interface} channel {channel}')
+            time.sleep(interval)
+        except KeyboardInterrupt:
+            break
 
-		# we are looking for management frames with a probe subtype
-		# if neither match we are done here
-		if packet.type != 0 or packet.subtype != 0x04:
-			return
+def signal_handler(procs, signal, frame):
+    for proc in procs:
+        logger.info('terminating %s', proc)
+        proc.terminate()
+        proc.join()
+    sys.exit(0)
 
-		# list of output fields
-		fields = []
 
-		# determine preferred time format 
-		log_time = str(int(time.time()))
-		if time_fmt == 'iso':
-			log_time = datetime.datetime.now().isoformat()
+def packet_callback(packet):
+    if not packet.haslayer(Dot11):
+     return
 
-		fields.append(log_time)
+    # we are looking for management frames with a probe subtype
+    # if neither match we are done here
+    if packet.type != 0 or packet.subtype != 0x04:
+     return
 
-		# append the mac address itself
-		fields.append(packet.addr2)
+    # list of output fields
+    fields = []
 
-		# parse mac address and look up the organization from the vendor octets
-		if mac_info:
-			try:
-				parsed_mac = netaddr.EUI(packet.addr2)
-				fields.append(parsed_mac.oui.registration().org)
-			except netaddr.core.NotRegisteredError, e:
-				fields.append('UNKNOWN')
+    # append the mac address itself
+    fields.append(packet.addr2)
 
-		# include the SSID in the probe frame
-		if ssid:
-			fields.append(packet.info)
-			
-		if rssi:
-			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
-			fields.append(str(rssi_val))
+    # parse mac address and look up the organization from the vendor octets
+    
+    try:
+     parsed_mac = netaddr.EUI(packet.addr2)
+     fields.append(parsed_mac.oui.registration().org)
+    except netaddr.core.NotRegisteredError as e:
+     fields.append('UNKNOWN')
 
-		logger.info(delimiter.join(fields))
+    # include the SSID in the probe frame
+    fields.append(packet.info)
+    
+    # signal strength in dbm
+    try:
+     fields.append(packet.dBm_AntSignal)
+    except Exception as e:
+     logger.error(e)
+    
+    try:
+     fields.append(packet.Channel)
+    except Exception as e:
+     logger.error(e)
 
-	return packet_callback
+    logger.info('\t'.join(map(str,fields)))
 
 def main():
-	parser = argparse.ArgumentParser(description=DESCRIPTION)
-	parser.add_argument('-i', '--interface', help="capture interface")
-	parser.add_argument('-t', '--time', default='iso', help="output time format (unix, iso)")
-	parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
-	parser.add_argument('-b', '--max-bytes', default=5000000, help="maximum log size in bytes before rotating")
-	parser.add_argument('-c', '--max-backups', default=99999, help="maximum number of log files to keep")
-	parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
-	parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
-	parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
-	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
-	parser.add_argument('-D', '--debug', action='store_true', help="enable debug output")
-	parser.add_argument('-l', '--log', action='store_true', help="enable scrolling live view of the logfile")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-i', '--interface', nargs='+', help="capture interface")
+    parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
+    parser.add_argument('-b', '--max-bytes', default=5000000, help="maximum log size in bytes before rotating")
+    parser.add_argument('-c', '--max-backups', default=99999, help="maximum number of log files to keep")
+    args = parser.parse_args()
 
-	if not args.interface:
-		print "error: capture interface not given, try --help"
-		sys.exit(-1)
-	
-	DEBUG = args.debug
+    if not args.interface:
+     print("error: at least one capture interface required, try --help")
+     sys.exit(-1)
 
-	# setup our rotating logger
-	logger = logging.getLogger(NAME)
-	logger.setLevel(logging.INFO)
-	handler = RotatingFileHandler(args.output, maxBytes=args.max_bytes, backupCount=args.max_backups)
-	logger.addHandler(handler)
-	if args.log:
-		logger.addHandler(logging.StreamHandler(sys.stdout))
-	built_packet_cb = build_packet_callback(args.time, logger, 
-		args.delimiter, args.mac_info, args.ssid, args.rssi)
-	sniff(iface=args.interface, prn=built_packet_cb, store=0)
+    # Start the channel hopper
+
+    hopper_func = partial(channel_hopper, args.interface, 2)
+
+    hopper_process = Process(target = hopper_func)
+    hopper_process.start()
+
+    procs = [hopper_process]
+    interfaces = args.interface
+    for interface in interfaces:
+        sniff_func = partial(sniff,iface=args.interface, prn=packet_callback, store=0)
+        sniff_proc = Process(target=sniff_func)
+        sniff_proc.start()
+        procs.append(sniff_proc)
+    
+    # Capture CTRL-C
+    signal_handler_func = partial(signal_handler, procs)
+    signal.signal(signal.SIGINT, signal_handler_func)
+    
 
 if __name__ == '__main__':
-	main()
+    main()
